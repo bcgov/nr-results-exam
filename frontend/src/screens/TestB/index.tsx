5d6a822b7a6bfb5c2fcedf5d3ac1253ea3df88e6
jazzgrewal <jaski.grewal@gmail.com>
2023-08-16 23:20:22 -0700

    added multiple components for multi test

---
+++ frontend/src/screens/TestB/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { logout } from "../../services/AuthService";
+import { Button } from "@carbon/react";
+import { Asleep, Light } from '@carbon/icons-react';
+import { useThemePreference } from "../../utils/ThemePreference";
+import { useSelector } from "react-redux";
+import { toggleTheme } from "../../utils/ThemeFunction";
+import TestComponent from "../../components/TestComponent";
+
+
+const TestB: React.FC = () => {
+  const userDetails = useSelector((state:any) => state.userDetails)
+  const { loading, error, user } = userDetails
+  
+  const { theme, setTheme } = useThemePreference();
+    return (
+      <>
+        {user?<TestComponent user={user.userInfo} testName="Test B" questionFileName="questions.yaml"/>:<><div className="h4">Null</div></>}
+      </>
+    );
+  };
+
+export default TestB;
