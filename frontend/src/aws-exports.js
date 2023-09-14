bc55f5e44f9cdced8d2b88ff94e37d97de557c7b
jazzgrewal <jaski.grewal@gmail.com>
2023-09-14 01:09:23 -0700

    aded the Caddyfile and changed the dockerfile anddocker compose to use caddy, along with the env.ts

--- frontend/src/aws-exports.js
+++ frontend/src/aws-exports.js
@@ -1,10 +1,11 @@
+import { env } from './env';
 const awsconfig = {
-    aws_cognito_region: process.env.REACT_APP_COGNITO_REGION,
-    aws_user_pools_id: process.env.REACT_APP_USER_POOLS_ID,
-    aws_user_pools_web_client_id: process.env.REACT_APP_USER_POOLS_WEB_CLIENT_ID,
+    aws_cognito_region: 'ca-central-1',
+    aws_user_pools_id: env.REACT_APP_USER_POOLS_ID,
+    aws_user_pools_web_client_id: env.REACT_APP_USER_POOLS_WEB_CLIENT_ID,
     aws_mandatory_sign_in: 'enable',
     oauth: {
-        domain: process.env.REACT_APP_AWS_DOMAIN,
+        domain: 'test-fam-user-pool-domain.auth.ca-central-1.amazoncognito.com',
         scope: ['openid'],
         redirectSignIn: "http://localhost:3000/dashboard",
         redirectSignOut: "https://logontest7.gov.bc.ca/clp-cgi/logoff.cgi?retnow=1&returl=https://dev.loginproxy.gov.bc.ca/auth/realms/standard/protocol/openid-connect/logout?redirect_uri=http://localhost:3000/",
