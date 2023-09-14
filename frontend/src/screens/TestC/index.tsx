d66e048c45a53cef140baf0fcd70b115d2d197e1
jazzgrewal <jaski.grewal@gmail.com>
2023-09-14 01:09:23 -0700

    aded the Caddyfile and changed the dockerfile anddocker compose to use caddy, along with the env.ts

--- frontend/src/screens/TestC/index.tsx
+++ frontend/src/screens/TestC/index.tsx
@@ -1,18 +1,11 @@
 import React from "react";
-import { logout } from "../../services/AuthService";
-import { Button } from "@carbon/react";
-import { Asleep, Light } from '@carbon/icons-react';
-import { useThemePreference } from "../../utils/ThemePreference";
 import { useSelector } from "react-redux";
-import { toggleTheme } from "../../utils/ThemeFunction";
 import TestComponent from "../../components/TestComponent";
 
 
 const TestC: React.FC = () => {
   const userDetails = useSelector((state:any) => state.userDetails)
-  const { loading, error, user } = userDetails
-  
-  const { theme, setTheme } = useThemePreference();
+  const { user } = userDetails
     return (
       <>
         {user?<TestComponent user={user} testName="Test C" questionFileName="questions/questionsC.yaml"/>:<><div className="h4">Null</div></>}
