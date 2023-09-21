7dc14ee5890e089272a5a6e2b23eeaeced851a4d
Derek Roberts <derek.roberts@gmail.com>
2023-09-21 10:24:24 -0700

    Copy upstream over conflicted files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
   BrowserRouter, Routes, Route
 } from 'react-router-dom';
@@ -59,39 +58,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-=======
-import Box from '@mui/material/Box'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
-import AppRoutes from '@/routes'
-import { BrowserRouter } from 'react-router-dom'
-
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh',
-  },
-  content: {
-    flexGrow: 1,
-    marginTop: '5em',
-    marginRight: '1em',
-    marginLeft: '1em',
-    marginBottom: '5em',
-    height: '100%',
-  },
-}
-export default function App() {
-  return (
-    <Box sx={styles.container}>
-      <Header />
-      <Box sx={styles.content}>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-      </Box>
-      <Footer />
-    </Box>
-  )
-}
->>>>>>> 2577926 (Initial commit)
