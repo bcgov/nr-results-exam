21ef8fae9c13e3150b10eb687a271c86f86b2390
Derek Roberts <derek.roberts@gmail.com>
2023-09-21 19:18:37 -0700

    bug: frontend build errors (#14)

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -6,4 +6,4 @@ declare global {
 }
 
 // eslint-disable-next-line import/prefer-default-export
-export const env: Record<string, any> = { ...import.meta.env, ...window.config }
+export const env: Record<string, any> = { ...import.meta, ...window.config }
