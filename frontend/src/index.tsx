0627a2fd99ce009afb50022ffd1d2d47ea79c634
jazzgrewal <jaski.grewal@gmail.com>
2023-09-22 00:56:28 -0700

    changed the nr-fsa-theme to nr-theme

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -13,7 +13,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <ClassPrefix prefix='bcgov'>
+    <ClassPrefix prefix='bx'>
       <ThemePreference>
         <Provider store={store}>
           <App />
