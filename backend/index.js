b36967d53f31f83efb1e9547322649840295949c
Jazz Grewal <39718912+jazzgrewal@users.noreply.github.com>
2023-10-05 11:49:01 -0700

    removed cors from express (#43)
    * removed cors from express
    
    * Caddyfile + VITE_AWS_DOMAIN
    
    ---------
    
    Co-authored-by: Derek Roberts <derek.roberts@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const dotenv =require('dotenv');
 const indexRoutes = require("./routes/indexRoutes");
 const mailRoutes = require("./routes/mailRoutes");
@@ -9,14 +8,7 @@ dotenv.config({
   path: './.env'
 })
 const app = express();
-
-// Configure CORS options
-const corsOptions = {
-  origin: ['http://localhost:3000', 'https://nr-results-exam-test-frontend.apps.silver.devops.gov.bc.ca/'],
-  methods: ['GET','POST'], // Specify the allowed HTTP methods
-};
 app.use(express.json());
-app.use(cors(corsOptions));
 
 app.use('/api/', indexRoutes);
 app.use('/api/mail', mailRoutes);
