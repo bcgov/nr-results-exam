73462836995a406c42f4cf736e3897182919da08
Jazz Grewal <39718912+jazzgrewal@users.noreply.github.com>
2023-10-10 19:56:05 -0700

    Test/cors local (#50)
    * added the ngrok frontend url
    
    * fixed by adding whitelist ip array
    
    * added origin in the deploy.yml
    
    * Add health check, let through CORS
    
    * .gitignore + **/.vscode
    
    * Cleanup
    
    * positioning the health route before defining strict CORS, and allow * for health
    
    * fixed the healthROute location
    
    ---------
    
    Co-authored-by: Derek Roberts <derek.roberts@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -3,22 +3,36 @@ const cors = require('cors');
 const dotenv =require('dotenv');
 const indexRoutes = require("./routes/indexRoutes");
 const mailRoutes = require("./routes/mailRoutes");
-const healthRoute = require('./routes/health');
+const healthRoutes = require("./routes/healthRoutes");
 
 dotenv.config({
   path: './.env'
 })
 const app = express();
-
-// Configure CORS options
-const corsOptions = {origin: '*'};
 app.use(express.json());
+// positioning the health route before defining strict CORS, and allow * for health
+app.use('/health',cors({origin:'*'}),healthRoutes);
+
+const whitelist = ['http://localhost:3000', 'https://nr-results-exam-test-frontend.apps.silver.devops.gov.bc.ca/', 'https://nr-results-exam-prod-frontend.apps.silver.devops.gov.bc.ca/' ];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (origin && whitelist.some(domain => origin.startsWith(domain))) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+// Let health check through before CORS
+app.use('/health', healthRoutes);
+
+// CORS, routes
 app.use(cors(corsOptions));
 
 app.use('/api/', indexRoutes);
 app.use('/api/mail', mailRoutes);
-
-app.use('/health', healthRoute);
 
 app.listen(5000, () => {
   console.log('Backend server is running on port 5000');
