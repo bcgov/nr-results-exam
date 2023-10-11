0a681d91624bba4916ee4062bd85898cfb87e43e
Jazz Grewal <39718912+jazzgrewal@users.noreply.github.com>
2023-10-10 19:56:05 -0700

    Test/cors local (#50)
    * added the ngrok frontend url
    
    * fixed by adding whitelist ip array
    
    * added origin in the deploy.yml
    
    * Add health check, let through CORS
    
    * .gitignore + **/.vscode
    
    * Cleanup
    
    * positioning the health route before defining strict CORS, and allow * for health
    
    * fixed the healthROute location
    
    ---------
    
    Co-authored-by: Derek Roberts <derek.roberts@gmail.com>

--- backend/routes/healthRoutes.js
+++ backend/routes/healthRoutes.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const router = express.Router({});
+
 router.get('/', async (_req, res, _next) => {
-    const check = {
+    res.send({
         uptime: process.uptime(),
         message: 'OK',
         timestamp: Date.now()
-    };
-    try {
-        res.send(check);
-    } catch (error) {
-        check.message = error;
-        res.status(503).send();
-    }
+    });
 });
-// export router with all routes included
+
 module.exports = router;
