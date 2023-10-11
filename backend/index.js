0d81f6841388d3cd6695718b3435df3bdeedffb0
jazzgrewal <jaski.grewal@gmail.com>
2023-10-10 19:38:05 -0700

    positioning the health route before defining strict CORS, and allow * for health

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ dotenv.config({
 })
 const app = express();
 app.use(express.json());
+// positioning the health route before defining strict CORS, and allow * for health
+app.use('/health',cors({origin:'*'}),healthRoute);
 
 const whitelist = ['http://localhost:3000', 'https://nr-results-exam-test-frontend.apps.silver.devops.gov.bc.ca/', 'https://nr-results-exam-prod-frontend.apps.silver.devops.gov.bc.ca/' ];
 
@@ -28,6 +30,7 @@ app.use('/health', healthRoutes);
 
 // CORS, routes
 app.use(cors(corsOptions));
+
 app.use('/api/', indexRoutes);
 app.use('/api/mail', mailRoutes);
 
