b83a5caed38fad61abedd36e7c97ae27299c346c
jazzgrewal <jaski.grewal@gmail.com>
2025-02-26 16:38:49 -0800

    resolved package dependencies

--- frontend/src/module.d.ts
+++ frontend/src/module.d.ts
@@ -2,6 +2,5 @@ declare module '@carbon/react';
 declare module '@carbon/icons-react';
 declare module '@carbon/pictograms-react';
 declare module 'aws-amplify';
-declare module '@aws-amplify/ui-react';
 declare module 'react-lottie';
 declare module '*.png';
