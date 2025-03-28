22c430938e3a3ff4059fd4382976f4531a000d09
Derek Roberts <derek.roberts@gmail.com>
2025-03-27 20:19:10 -0700

    Pass in frontend url

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,12 @@ app.use(express.json());
 // positioning the health route before defining strict CORS, and allow * for health
 app.use('/health',cors({origin:'*'}),healthRoutes);
 
+const frontendUrl = process.env.FRONTEND_URL;
 const whitelist = [
   'http://localhost:3000',
   'https://nr-results-exam-test-frontend.apps.silver.devops.gov.bc.ca',
   'https://nr-results-exam-prod-frontend.apps.silver.devops.gov.bc.ca',
-  'https://*.apps.silver.devops.gov.bc.ca'
+  frontendUrl
 ];
 
 const corsOptions = {
